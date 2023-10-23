9f1cf0a558cc3f712e0dd250dea0eb63ae5b4b0e
JulioEVA <julio-emmanuel2011@hotmail.com>
2023-10-22 17:04:26 -0700

    Hotfix: Change currentUser initialization from an empty string to an empty object

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,7 @@ function App() {
   const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false);
   const [isImagePopupOpen, setIsImagePopupOpen] = React.useState(false);
   const [selectedCard, setSelectedCard] = React.useState();
-  const [currentUser, setCurrentUser] = React.useState("");
+  const [currentUser, setCurrentUser] = React.useState({});
   const [cards, setCards] = React.useState([]);
 
   /**
